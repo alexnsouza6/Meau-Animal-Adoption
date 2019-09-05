82dfb7a92bd35a5a18ec53e586ec00737b78c48b
joao96 <jvpoletti@gmail.com>
2019-09-05 11:27:33 -0300

    :wrench: minor firebase configuration fix.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,6 @@
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/database';
 
-
 const firebaseConfig = {
   apiKey: 'AIzaSyAOCemXaF-cVi9CB0Pjp6_OT5_y5b2MMWQ',
   authDomain: 'meau-app-8f1b1.firebaseapp.com',
@@ -12,6 +11,6 @@ const firebaseConfig = {
   appId: '1:821505612282:web:e3ea38dc9ee8ffec086c43',
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
+export default !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
