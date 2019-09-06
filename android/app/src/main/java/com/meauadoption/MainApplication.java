91dba350e03649fba510ba12ba7c101cd2108169
joao96 <jvpoletti@gmail.com>
2019-09-06 14:46:12 -0300

    :wrench: configuring fb sdk and login for android.

--- android/app/src/main/java/com/meauadoption/MainApplication.java
+++ android/app/src/main/java/com/meauadoption/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -26,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      // packages.add(new FBSDKPackage());
       return packages;
     }
 
