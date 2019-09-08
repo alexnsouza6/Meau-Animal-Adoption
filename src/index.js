9558a0bb26dc3489be9a654ae0240f36b3561dfd
joao96 <jvpoletti@gmail.com>
2019-09-08 20:25:20 -0300

    :art: adding store provider to Route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-
+import { Provider } from 'react-redux';
 import Routes from './routes';
-
 
 if (__DEV__) {
   import('../ReactotronConfig');
 }
 
+import configureStore from './store';
+
+const store = configureStore()
+
 const App = () => (
   <>
     <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
-    <Routes />
+    <Provider store = { store }>
+      <Routes />
+    </Provider>
   </>
 );
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Main from './pages/Main';
 import Loading from './components/Loading';
 import SignUp from './containers/SignUp';
 import Register from './containers/Register';
+import Profile from './containers/Profile';
 import Login from './containers/Login';
 
 const Routes = createAppContainer(createSwitchNavigator(
@@ -11,6 +12,7 @@ const Routes = createAppContainer(createSwitchNavigator(
     Loading,
     SignUp,
     Register,
+    Profile,
     Login,
     Main,
   },
