c6b665019e543812945732215cbf33161dcfe7d3
joao96 <jvpoletti@gmail.com>
2019-09-16 20:35:48 -0300

    :art: adding color to statusBar

--- src/components/ScreenHeader/index.js
+++ src/components/ScreenHeader/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import { StatusBar } from 'react-native';
 import { Header, HeaderContainer, HeaderText } from './style';
 
 
@@ -17,6 +17,7 @@ class ScreenHeader extends React.Component {
     const { title, color } = this.props;
     return (
       <HeaderContainer style={{ backgroundColor: color }}>
+        <StatusBar backgroundColor="#f7a800" barStyle="light-content" />
         <Header>
           <Icon name="menu" size={28} color="#434343" onPress={this.toggleDrawer.bind(this)} />
           <HeaderText>{ title }</HeaderText>
