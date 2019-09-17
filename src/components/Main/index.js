ec2e57a57c8e9155e901eb3be61709be0192bc36
joao96 <jvpoletti@gmail.com>
2019-09-16 21:18:49 -0300

    :art: minor adjustment

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -12,36 +12,38 @@ import {
 const meauImage = require('../../assets/images/Meau_marca_2.png');
 
 const Main = ({ navigation }) => (
-  <Container>
+  <>
     <ScreenHeader title="Meau" color="#ffd358" navigationProps={navigation} />
-    <Title> Olá! </Title>
-    <DescriptionContainer>
-      <Description>Bem vindo ao Meau!</Description>
-      <Description>Aqui você pode adotar, doar e ajudar cães e gatos com facilidade.</Description>
-      <Description>Qual o seu interesse?</Description>
-    </DescriptionContainer>
-    <Menu>
-      <MenuButton onPress={() => { navigation.navigate('Feed'); }}>
-        <MenuText>
-          ADOTAR
-        </MenuText>
-      </MenuButton>
-      <MenuButton>
-        <MenuText>
-          AJUDAR
-        </MenuText>
-      </MenuButton>
-      <MenuButton>
-        <MenuText>
-          CADASTRAR ANIMAL
-        </MenuText>
-      </MenuButton>
-    </Menu>
-    <LoginButton onPress={() => { navigation.navigate('Login'); }} />
-    <RegisterButton onPress={() => { navigation.navigate('Registrar'); }} />
+    <Container>
+      <Title> Olá! </Title>
+      <DescriptionContainer>
+        <Description>Bem vindo ao Meau!</Description>
+        <Description>Aqui você pode adotar, doar e ajudar cães e gatos com facilidade.</Description>
+        <Description>Qual o seu interesse?</Description>
+      </DescriptionContainer>
+      <Menu>
+        <MenuButton onPress={() => { navigation.navigate('Feed'); }}>
+          <MenuText>
+            ADOTAR
+          </MenuText>
+        </MenuButton>
+        <MenuButton>
+          <MenuText>
+            AJUDAR
+          </MenuText>
+        </MenuButton>
+        <MenuButton>
+          <MenuText>
+            CADASTRAR ANIMAL
+          </MenuText>
+        </MenuButton>
+      </Menu>
+      <LoginButton onPress={() => { navigation.navigate('Login'); }} />
+      <RegisterButton onPress={() => { navigation.navigate('SignUp'); }} />
 
-    <Logo source={meauImage} />
-  </Container>
+      <Logo source={meauImage} />
+    </Container>
+  </>
 );
 
 Main.propTypes = {
